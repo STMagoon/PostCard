584ffdaa896ec5d726d676596cdf1d2f3c82711d
STMagoon <Stephen@s1.stmnet.us>
2015-01-04 20:30:35 -0700

    added name label and code to set it

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mailButton: UIButton!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -36,9 +38,15 @@ class ViewController: UIViewController {
         messagelabel.text = enterMessageTextField.text
         messagelabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
